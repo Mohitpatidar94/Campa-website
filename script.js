1675f1848d9efe4743fc70af8a3474bbe9ee1fa5
Mohit <167812581+Mohitpatidar94@users.noreply.github.com>
2025-03-19 19:08:10 -1200

    Update script.js

--- script.js
+++ script.js
@@ -14,15 +14,19 @@ let circle2 = document.querySelector("#circle2");
 circle2.addEventListener("click",()=>{
     changeImg.src = "2nd cane.png";
     spanText.style.color="rgb(214, 176, 79)"
-    btn.style.background="rgb(214, 176, 79)"
+    btn.style.background=" rgb(214, 176, 79)"
 })
 
 let circle3 = document.querySelector("#circle3");
 circle3.addEventListener("click",()=>{
       changeImg.src = "3rd cane.png";
+     spanText.style.color="rgb(219, 118, 35)"
+    btn.style.background=" rgb(219, 118, 35)"
 })
 
 let circle4 = document.querySelector("#circle4");
 circle4.addEventListener("click",()=>{
    changeImg.src = "4th cane.png";
+     spanText.style.color="rgb(187, 189, 191)"
+    btn.style.background=" rgb(187, 189, 191)"
 })
